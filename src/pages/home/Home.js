9c13bf98b59bdd6d4c1ff91432054899fc126433
Yavuz Avci <yavuzavci01@gmail.com>
2023-05-24 13:18:58 +0300

    products added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css';
+import ProductCard from '../../components/ProductCard';
 
 const Home = () => {
+    const [tarifler, setTarifler] = useState(null);
+    useEffect(() => {
+        fetch("http://localhost:3000/tarifler")
+            .then(res => res.json())
+            .then(data => setTarifler(data))
+    }, [])
     return (
-        <div>Home</div>
+
+        <div className="row mt-3">
+            {
+                tarifler && tarifler.map((tarif, index) => (
+                    <ProductCard key={index} tarif={tarif} />
+                ))
+            }
+        </div>
     )
 }
 
